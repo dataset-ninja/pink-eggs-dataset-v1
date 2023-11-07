07e76781be3f1947c7295dc7142304e3a153f85b
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-07 21:53:32 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -43,7 +43,7 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/pink-eggs-dataset-v1"
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = ["https://www.kaggle.com/datasets/deeshenzhen/pinkeggs"]
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/deeshenzhen/pinkeggs"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
